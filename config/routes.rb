08f7e7f6ea7dd02b98dc72f87b3b5fd71f04a461
gus1000 <gramirez644@gmail.com>
2025-05-07 08:58:17 -0700

    fixed index method

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # get "/users/current", to: "users#current"
+
   namespace :api do 
     post 'generate_disclaimer', to: 'openai#generate'
 
