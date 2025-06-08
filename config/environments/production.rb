1e78edea21655063ffc543b602b77d671e76405f
gus1000 <gramirez644@gmail.com>
2025-06-08 15:45:13 -0700

    Add RAILS_ENV debug print to trace environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,9 @@
 require "active_support/core_ext/integer/time"
 
+
+abort("🛑 Running in #{Rails.env} mode — aborting to verify environment!")
+
+
 #expermenting
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
