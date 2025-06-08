31c0c30aa460a6cc024e353bbacc747e015f89d0
gus1000 <gramirez644@gmail.com>
2025-06-08 15:22:03 -0700

    Test: log when production.rb is loaded

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,6 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  puts "🧪 production.rb is loaded — eager_load = #{config.eager_load}"
+
 end
