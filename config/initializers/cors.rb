88cf90bb329ad581019cdf9ad5e8f1b394b992c9
gus1000 <gramirez644@gmail.com>
2025-06-14 07:59:59 -0700

    added frotnend widget to CORS file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins %r{\Ahttp://localhost:\d+\z}, 'https://independent-fulfillment-production.up.railway.app'
-
+    origins %r{\Ahttp://localhost:\d+\z},
+            'https://independent-fulfillment-production.up.railway.app',
+            'https://widget-production-a68c.up.railway.app' # ✅ this is your frontend app
     resource '*',
       headers: :any,
-      credentials: true,   
+      credentials: true,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
